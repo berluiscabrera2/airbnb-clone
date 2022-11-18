5d30983d65f2c4eb11257ef6669f3b84e936f1a0
Berluis Cabrera <bcmena02@gmail.com>
2022-11-17 22:42:33 -0400

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Home from './Home'
 import Header from './Header'
+import Footer from './Footer'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
@@ -9,6 +11,7 @@ function App() {
     <div className='app'>
         <Header/>
         <Home/>
+        <Footer />
         {/*Home*/}
         
 
